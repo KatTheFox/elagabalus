ecee454a7f8af2db70543d08ee0a35823aa1ad5a
Katrina Renarde <katthefoxmaid@gmail.com>
2023-04-13 15:21:27 -0700

    fix:formatting

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-import { tokens} from "./config.json"
-import { Client, Events, GatewayIntentBits } from "discord.js"
-import { commands } from "./commands/commands"
-const client=new Client({intents:[GatewayIntentBits.Guilds]})
-client.once(Events.ClientReady,(c)=>{
-    console.log(`Logged in as ${c.user.tag}`)
-})
-client.on(Events.InteractionCreate,async interaction=>{
-    if(!interaction.isChatInputCommand())return;
-    const command=commands.get(interaction.commandName)
-    if(command==undefined){
-        console.error(`no such command ${interaction.commandName}`)
-        return
-    }
-    try{
-        await command.execute(interaction)
-    } catch (e){
-        console.error(e);
-        await interaction.reply({content:"There was an error executing this command",ephemeral:true})
-    }
-    
-})
-client.login(tokens.token)
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { commands } from "./commands/commands";
+import { tokens } from "./config.json";
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, (c) => {
+  console.log(`Logged in as ${c.user.tag}`);
+});
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = commands.get(interaction.commandName);
+  if (command == undefined) {
+    console.error(`no such command ${interaction.commandName}`);
+    return;
+  }
+  try {
+    await command.execute(interaction);
+  } catch (e) {
+    console.error(e);
+    await interaction.reply({
+      content: "There was an error executing this command",
+      ephemeral: true,
+    });
+  }
+});
+client.login(tokens.token);
