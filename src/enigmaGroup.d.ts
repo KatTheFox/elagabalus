9c2ee004532f71eab4d83e879f6ed52a5ea85cad
Katrina Renarde <katthefoxmaid@gmail.com>
2023-10-06 16:35:12 -0700

    feat: started work on The Three and the Three

---
+++ src/enigmaGroup.d.ts
@@ -0,0 +1,12 @@
+declare type Enigma = {
+  id: string;
+  degrees: EnigmaDegree[];
+};
+declare type EnigmaDegree = {
+  id: string;
+  answer: string;
+  title: string;
+  text: string;
+  image1: string;
+  image2: string;
+};
